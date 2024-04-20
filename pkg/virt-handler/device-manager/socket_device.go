fa0267a9c63011bc796c3e375fcc7a569372aa8a
aerosouund <aerosound161@gmail.com>
2024-04-20 14:15:16 +0200

    registerfunc on socket

--- pkg/virt-handler/device-manager/socket_device.go
+++ pkg/virt-handler/device-manager/socket_device.go
@@ -23,16 +23,13 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"net"
 	"os"
-	"path"
 	"path/filepath"
 	"strconv"
 	"strings"
 	"sync"
 
 	"github.com/fsnotify/fsnotify"
-	"google.golang.org/grpc"
 
 	"kubevirt.io/client-go/log"
 
@@ -44,10 +41,9 @@ import (
 
 type SocketDevicePlugin struct {
 	DevicePluginBase
-	pluginSocketPath string
-	socketDir        string
-	socket           string
-	socketName       string
+	socketDir  string
+	socket     string
+	socketName string
 }
 
 func NewSocketDevicePlugin(socketName, socketDir, socket string, maxDevices int) *SocketDevicePlugin {
@@ -57,12 +53,16 @@ func NewSocketDevicePlugin(socketName, socketDir, socket string, maxDevices int)
 			resourceName: fmt.Sprintf("%s/%s", DeviceNamespace, socketName),
 			initialized:  false,
 			lock:         &sync.Mutex{},
+			done:         make(chan struct{}),
+			deregistered: make(chan struct{}),
+			socketPath:   SocketPath(strings.Replace(socketName, "/", "-", -1)),
 		},
-		pluginSocketPath: SocketPath(strings.Replace(socketName, "/", "-", -1)),
-		socket:           socket,
-		socketDir:        socketDir,
-		socketName:       socketName,
+		socket:     socket,
+		socketDir:  socketDir,
+		socketName: socketName,
 	}
+	dpi.registerFunc = dpi.registerServer
+	dpi.healthcheck = dpi.healthCheck
 	for i := 0; i < maxDevices; i++ {
 		deviceId := dpi.socketName + strconv.Itoa(i)
 		dpi.devs = append(dpi.devs, &pluginapi.Device{
@@ -71,81 +71,6 @@ func NewSocketDevicePlugin(socketName, socketDir, socket string, maxDevices int)
 		})
 	}
 	return dpi
-}
-
-func (dpi *SocketDevicePlugin) GetDeviceName() string {
-	return dpi.socketName
-}
-
-// Start starts the device plugin
-func (dpi *SocketDevicePlugin) Start(stop <-chan struct{}) (err error) {
-	logger := log.DefaultLogger()
-	dpi.stop = stop
-	dpi.done = make(chan struct{})
-	dpi.deregistered = make(chan struct{})
-
-	err = dpi.cleanup()
-	if err != nil {
-		return err
-	}
-
-	sock, err := net.Listen("unix", dpi.pluginSocketPath)
-	if err != nil {
-		return fmt.Errorf("error creating GRPC server socket: %v", err)
-	}
-
-	dpi.server = grpc.NewServer([]grpc.ServerOption{}...)
-	defer dpi.stopDevicePlugin()
-
-	pluginapi.RegisterDevicePluginServer(dpi.server, dpi)
-
-	errChan := make(chan error, 2)
-
-	go func() {
-		errChan <- dpi.server.Serve(sock)
-	}()
-
-	err = waitForGRPCServer(dpi.pluginSocketPath, connectionTimeout)
-	if err != nil {
-		return fmt.Errorf("error starting the GRPC server: %v", err)
-	}
-
-	err = dpi.register()
-	if err != nil {
-		return fmt.Errorf("error registering with device plugin manager: %v", err)
-	}
-
-	go func() {
-		errChan <- dpi.healthCheck()
-	}()
-
-	dpi.setInitialized(true)
-	logger.Infof("%s device plugin started", dpi.socketName)
-	err = <-errChan
-
-	return err
-}
-
-// Register registers the device plugin for the given resourceName with Kubelet.
-func (dpi *SocketDevicePlugin) register() error {
-	conn, err := gRPCConnect(pluginapi.KubeletSocket, connectionTimeout)
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-
-	client := pluginapi.NewRegistrationClient(conn)
-	reqt := &pluginapi.RegisterRequest{
-		Version:      pluginapi.Version,
-		Endpoint:     path.Base(dpi.pluginSocketPath),
-		ResourceName: dpi.resourceName,
-	}
-
-	_, err = client.Register(context.Background(), reqt)
-	if err != nil {
-		return err
-	}
-	return nil
 }
 
 func (dpi *SocketDevicePlugin) Allocate(ctx context.Context, r *pluginapi.AllocateRequest) (*pluginapi.AllocateResponse, error) {
@@ -181,12 +106,8 @@ func (dpi *SocketDevicePlugin) Allocate(ctx context.Context, r *pluginapi.Alloca
 	return &response, nil
 }
 
-func (dpi *SocketDevicePlugin) cleanup() error {
-	if err := os.Remove(dpi.pluginSocketPath); err != nil && !errors.Is(err, os.ErrNotExist) {
-		return err
-	}
-
-	return nil
+func (dpi *SocketDevicePlugin) registerServer() {
+	pluginapi.RegisterDevicePluginServer(dpi.server, dpi)
 }
 
 func (dpi *SocketDevicePlugin) healthCheck() error {
@@ -215,13 +136,13 @@ func (dpi *SocketDevicePlugin) healthCheck() error {
 	}
 	logger.Infof("device '%s' is present.", devicePath)
 
-	dirName := filepath.Dir(dpi.pluginSocketPath)
+	dirName := filepath.Dir(dpi.socketPath)
 	err = watcher.Add(dirName)
 
 	if err != nil {
 		return fmt.Errorf("failed to add the device-plugin kubelet path to the watcher: %v", err)
 	}
-	_, err = os.Stat(dpi.pluginSocketPath)
+	_, err = os.Stat(dpi.socketPath)
 	if err != nil {
 		return fmt.Errorf("failed to stat the device-plugin socket: %v", err)
 	}
@@ -243,7 +164,7 @@ func (dpi *SocketDevicePlugin) healthCheck() error {
 					logger.Infof("monitored device %s disappeared", dpi.socketName)
 					dpi.health <- deviceHealth{Health: pluginapi.Unhealthy}
 				}
-			} else if event.Name == dpi.pluginSocketPath && event.Op == fsnotify.Remove {
+			} else if event.Name == dpi.socketPath && event.Op == fsnotify.Remove {
 				logger.Infof("device socket file for device %s was removed, kubelet probably restarted.", dpi.socketName)
 				return nil
 			}
